2c329e786df3f6b8ccc7e9d1c07e2a93c11533e0
ValiVKhan <31572013+ValiVKhan@users.noreply.github.com>
2020-11-03 23:37:52 -0600

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,149 +1,81 @@
+import speech_recognition as sr
+from hanger import hanger
 from time import sleep
 import random
 
-hanger=['''
-                 _____
-                |     |
-                      |
-                      |
-                      |
-                     _|_''', '''
-                 _____
-                |     |
-                O     |
-                      |
-                      |
-                     _|_''', '''
-                 _____
-                |     |
-                O     |
-                |     |
-                |     |
-                     _|_''', '''
-                 _____
-                |     |
-                O     |
-               /|     |
-                |     |
-                     _|_''', '''
-                 _____
-                |     |
-                O     |
-               /|\    |
-                |     |
-                     _|_''', ''' 
-                 _____
-                |     |
-                O     |
-               /|\    |
-                |     |
-               /     _|_''', '''
-                 _____
-                |     |
-                O     |
-      ~Loser~  /|\    |  ~Loser~ 
-                |     |
-               / \   _|_''','''
-       ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆
-       ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆    
-                
-                    \O/      
-          ~WINNER~   |   ~WINNER~        
-                     |    
-                    / \ 
-                                       
-       ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆
-       ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆ ☆''']
 
-#Variable assign
-#Wrong is what contorls the graphics
-global wrong
-wrong = -1
-
-#The word being found
-name_number = (random.SystemRandom().randint(0, 212))
-file_open = open('wrds.txt', 'r')
+name_number = (random.SystemRandom().randint(0, 211))
+file_open = open('words.txt', 'r')
 
 all_lines = file_open.readlines()
 
 random_name = (all_lines[name_number])
 word = random_name
 print(word)
-#Loser count controls the break out of the loop is the player never guesses correctly and loses
-global losercount
-losercount = 0
-#Asking for a letter, this is in function becasue it is used repetitvley
+wordlength = (len(word))
 
+wordarray = ["_"]*(wordlength - 1)
+
+global hangershow
+hangershow = 0
+used=[]
 def asker():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak Anything :")
+        audio = r.listen(source) or input("what letter?")
+        try:
+            text = r.recognize_google(audio)
+            letter = (format(text))
+            if letter.__contains__("letter"):
+                global guess
+                guess = (letter[7]).lower()
+                if guess not in used:
+                    print(guess)
+                    print("\n")
+                    used.append(guess)
+                    print (' ' .join(used))
+                else:
+                    print("letter already been used before!")
+                    asker()
+            else:
+                asker()
+        except:
+            print("\n")
+
+
+def searcher():
+    letter =  0
     global guess
-    guess =  (input("What is your letter guess? "))
-
-
-#The lenght of the world, later used to see the amount of blanks
-global wordlenth
-wordlenth = (len(word))
-
-#The amount of blanks needed
-global blank
-wordwrite = wordlenth - 1
-blank = ["_"]*wordwrite
-global calc
-calc = 1
-# Finder runs through each character of the word and searches for the guess letter that was inputted.
-def finder():
-    Letter_list.append(guess)
-    #Letter is used when searching through the word
-    letter = 0
-    #Wroner is a counter which is only counted for when the guess letter is not found in a postion of the word
-    wroner = 0
-    while letter < wordlenth:
+    global wrongincount
+    wrongincount = 0
+    while letter < wordlength:
         if guess == (word[letter]):
-            #print(word[letter])
-            #print (letter)
-            blank[letter] = guess
-            #print(blank)
-            letter = letter + 1
-            global calc
-            calc = calc + 1
+            wordarray[letter] = guess
+            letter +=1
         else:
-            letter = letter + 1
-            wroner = wroner + 1
-            global losercount
-            losercount = losercount + 1
-    if wroner == wordlenth:
-        global wrong
-        wrong = wrong + 1
-        #wrong counts what graphic to display from the array hanger
-        if wrong != 7:
-            print(hanger[wrong])
-    #converts array to a string
-    print ('' .join(blank))
-    global blanks
-    blanks = (''.join(blank))
+            letter+=1
+            wrongincount +=1
+            continue
+    if wrongincount == wordlength:
+        global hangershow
+        hangershow +=1
+        print(hanger[hangershow])
+    print ('' .join(wordarray))
 
-#asker and finder are the  fucntions and must be ran before once because they give calues for the loop
-Letter_list = []
+
 asker()
-finder()
-#The while loop controlling everything
+searcher()
+global combined
+combined = ('' .join(wordarray))
+
 while 1:
-    #If the blanks dont equal the word ask for another letter and run finder
-    if blanks != word:
-        asker()
-        if guess in Letter_list:
-            print("Letter has already been used before!")
-        else:
-            finder()
-        #If los
-    if wrong == 6:
-        print(word)
-        break
-    if calc == wordlenth:
-        #Prints winner when the blank and word match
+    if combined == word:
         print(hanger[7])
         break
-
-
-
-    
-
+    elif hangershow < 6:  
+        asker()
+        searcher()
+    if hangershow == 6:
+        print(word)
+        break    
