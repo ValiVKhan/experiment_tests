9ea8a960779e9573d276dc6ac2a511f37a0c3833
ValiVKhan <31572013+ValiVKhan@users.noreply.github.com>
2020-11-22 16:04:02 -0600

    Update randomgen.py

--- randomgen.py
+++ randomgen.py
@@ -9,30 +9,45 @@ number_lenght = int(input("Length? "))
 
 global run
 run = 0
+global f
+f = open("random.txt", "w")
 while 1:
     if run == count:
         break
     else:
         if number_lenght == 1:
-            print((random.SystemRandom().randint(1, 9)))
+            f.write((str(random.SystemRandom().randint(1, 9))))
+            f.write("\n")
         if number_lenght == 2:
-            print((random.SystemRandom().randint(10, 99)))
+            f.write((str(random.SystemRandom().randint(10, 99))))
+            f.write("\n")
         if number_lenght == 3:
-            print((random.SystemRandom().randint(100, 999)))
+            f.write((str(random.SystemRandom().randint(100, 999))))
+            f.write("\n")
         if number_lenght == 4:
-            print((random.SystemRandom().randint(1000, 9999)))
+            f.write((str(random.SystemRandom().randint(1000, 9999))))
+            f.write("\n")
         if number_lenght == 5:
-            print((random.SystemRandom().randint(10000, 99999)))
+            f.write((str(random.SystemRandom().randint(10000, 99999))))
+            f.write("\n")
         if number_lenght == 6:
-            print((random.SystemRandom().randint(100000, 999999)))
+            f.write((str(random.SystemRandom().randint(100000, 999999))))
+            f.write("\n")
         if number_lenght == 7:
-            print((random.SystemRandom().randint(1000000, 9999999)))
+            f.write((str(random.SystemRandom().randint(1000000, 9999999))))
+            f.write("\n")
         if number_lenght == 8:
-            print((random.SystemRandom().randint(10000000, 99999999)))
+            f.write((str(random.SystemRandom().randint(10000000, 99999999))))
+            f.write("\n")
         if number_lenght == 9:
-            print((random.SystemRandom().randint(100000000, 999999999)))
+            f.write((str(random.SystemRandom().randint(100000000, 999999999))))
+            f.write("\n")
         if number_lenght == 10:
-            print((random.SystemRandom().randint(1000000000, 9999999999)))
+            f.write((str(random.SystemRandom().randint(1000000000, 9999999999))))
+            f.write("\n")
         if number_lenght == 11:
-            print((random.SystemRandom().randint(10000000000, 99999999999)))
+            f.write((str(random.SystemRandom().randint(10000000000, 99999999999))))
+            f.write("\n")
     run = run + 1
+    
+    
